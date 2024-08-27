53206f44801e06b08e853caf2aa167d6252788a8
bigacz <sbigacz@gmail.com>
2024-08-27 15:37:31 +0200

    add minification of css

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,10 @@
 const path = require('path');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   extends: path.resolve(__dirname, './webpack.common.js'),
   mode: 'production',
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
 };
